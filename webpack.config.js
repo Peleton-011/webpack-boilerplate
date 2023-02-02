0cebd527041d62f0bafb8669ba69260f426e2040
Nico <nicodiazan@gmail.com>
2023-02-02 11:55:59 +0100

    Add management of data files with custom parsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
+
 module.exports = {
     mode: "development",
     entry: {
@@ -29,6 +33,27 @@ module.exports = {
             {
                 test: /\.xml$/i,
                 use: ["xml-loader"],
+            },
+            {
+                test: /\.json5$/i,
+                type: "json",
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+            {
+                test: /\.yaml$/i,
+                type: "json",
+                parser: {
+                    parse: yaml.parse,
+                },
+            },
+            {
+                test: /\.toml$/i,
+                type: "json",
+                parser: {
+                    parse: toml.parse,
+                },
             },
         ],
     },
