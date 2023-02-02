3178669647056950ab1db3e003111c6c54831534
Nico <nicodiazan@gmail.com>
2023-02-02 22:40:53 +0100

    Add some production only optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,95 @@
-
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const toml = require("toml");
 const yaml = require("yamljs");
 const json5 = require("json5");
 
-module.exports = {
-    mode: "development",
-    entry: {
-        app: "./src/index.js",
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-    },
-    devtool: "eval-source-map", //for production, use none
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot|otf)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ["csv-loader"],
-            },
-            {
-                test: /\.xml$/i,
-                use: ["xml-loader"],
-            },
-            {
-                test: /\.json5$/i,
-                type: "json",
-                parser: {
-                    parse: json5.parse,
+const isProductionMode = (mode) => mode === "production";
+const currMode = "development"; // production
+
+module.exports = () => {
+    // This code executes before module.exports is defined.
+    console.log("🛎🛎🛎 isProduction", isProductionMode(nodeEnv));
+
+    return {
+        mode: currMode,
+        entry: "./src/index.js",
+        devtool: "eval-source-map", //for production, use none
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
                 },
-            },
-            {
-                test: /\.yaml$/i,
-                type: "json",
-                parser: {
-                    parse: yaml.parse,
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                    type: "asset/resource",
                 },
-            },
-            {
-                test: /\.toml$/i,
-                type: "json",
-                parser: {
-                    parse: toml.parse,
+                {
+                    test: /\.(woff|woff2|ttf|eot|otf)$/i,
+                    type: "asset/resource",
                 },
-            },
-        ],
-    },
-    devServer: {
-        static: "./dist", //The static directory where the server gets the data
-        hot: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Sample",
-        }),
-    ],
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    optimization: {
-        innerGraph: true,
-        usedExports: true,
-        splitChunks: {
-            chunks: "all",
+                {
+                    test: /\.(csv|tsv)$/i,
+                    use: ["csv-loader"],
+                },
+                {
+                    test: /\.xml$/i,
+                    use: ["xml-loader"],
+                },
+                {
+                    test: /\.json5$/i,
+                    type: "json",
+                    parser: {
+                        parse: json5.parse,
+                    },
+                },
+                {
+                    test: /\.yaml$/i,
+                    type: "json",
+                    parser: {
+                        parse: yaml.parse,
+                    },
+                },
+                {
+                    test: /\.toml$/i,
+                    type: "json",
+                    parser: {
+                        parse: toml.parse,
+                    },
+                },
+            ],
         },
-    },
+        devServer: {
+            static: "./dist", //The static directory where the server gets the data
+            hot: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Sample",
+            }),
+        ],
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+            clean: true,
+        },
+        optimization: {
+            minimize: isProductionMode(nodeEnv),
+            minimizer: isProductionMode(nodeEnv)
+               ? [new TerserPlugin(), new CssMinimizerPlugin()]
+               : [],
+            innerGraph: true,
+            usedExports: true,
+            splitChunks: {
+                chunks: "all",
+            },
+        },
+    };
 };
 
 //https://webpack.js.org/guides/
