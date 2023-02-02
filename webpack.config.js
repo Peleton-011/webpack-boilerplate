59aa786998949cf2802f04b9c097ccabaf5b0c01
Nico <nicodiazan@gmail.com>
2023-02-02 12:02:11 +0100

    Add output management functionality (HTML output and such)

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Output Management",
+            title: "Sample",
         }),
     ],
     output: {
